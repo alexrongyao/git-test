f7fe25702c59cacad838417b727c7ecd3e93b762
alexrongyao <99721546+alexrongyao@users.noreply.github.com>
2022-11-04 14:12:51 +0800

    Update GitTest.java
    pull commit

--- src/main/java/com/alex/GitTest.java
+++ src/main/java/com/alex/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world 3");
         System.out.println("master test");
         System.out.println("hot-fix text");
+        System.out.println("pull test");
     }
 }
